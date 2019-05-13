f3c12945654b771de5b2f48990b1adfee9e31069
Eric Reeves <leagues58@gmail.com>
2019-05-13 18:07:04 -0400

    Added scaffolded authentication pages

--- PowerserveHub/PowerserveHub/Models/ProductType.cs
+++ PowerserveHub/PowerserveHub/Models/ProductType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PowerserveHub.Models
+﻿namespace PowerserveHub.Models
 {
     public class ProductType
     {
