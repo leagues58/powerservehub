0f0e91fc1ad47f2b8821e4a63d8dab4c6b770b6b
Eric Reeves <leagues58@gmail.com>
2019-05-13 18:30:25 -0400

    Added Item request controller and view

---
+++ PowerserveHub/PowerserveHub/Controllers/ItemRequestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerserveHub.Controllers
+{
+    public class ItemRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
